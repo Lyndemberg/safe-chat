497e285cea6ad3dc1cae6121e5a5d06e7ae03566
Lyndemberg <lyndembergbatista@outlook.com>
2018-02-22 23:19:40 -0300

    subindo projeto

---
+++ src/main/java/com/ifpb/seguranca/safechat/controllers/ControladorUsuario.java
@@ -0,0 +1,94 @@
+
+package com.ifpb.seguranca.safechat.controllers;
+
+import com.ifpb.seguranca.safechat.dao.UsuarioDao;
+import com.ifpb.seguranca.safechat.model.Usuario;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author lyndemberg
+ */
+@Named
+@RequestScoped
+public class ControladorUsuario{
+    private Part foto;
+    private Usuario usuario = new Usuario();
+    @EJB
+    private UsuarioDao usuarioDao;
+    
+    public String cadastrar(){
+        if(usuarioDao.lerPorEmail(usuario.getEmail()) != null){
+            mostrarMsg("Cadastro", "Já existe um usuário com esse email");
+        }else{
+            byte[] fotoArray = new byte[ (int) foto.getSize()];
+            try {
+                foto.getInputStream().read(fotoArray);
+            } catch (IOException ex) {
+                Logger.getLogger(ControladorUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            usuario.setFoto(fotoArray);
+            usuarioDao.salvar(usuario);
+            return "index.xhtml";
+        }
+        return null;
+    }
+    public String logar(){
+        Usuario u = usuarioDao.lerPorEmail(usuario.getEmail());
+        if(u == null){
+            mostrarMsg("Login","Usuário não cadastrado");
+            return null;
+        }else{
+            Usuario autentica = usuarioDao.autentica(usuario.getEmail(), usuario.getSenha());
+            if(autentica == null){
+                mostrarMsg("Login","Dados incorretos");
+                return null;   
+            }else{
+                HttpSession sessao = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+                sessao.setAttribute("usuario", u);
+                return "home.xhtml";
+            }
+        }
+    }
+    public String logout(){
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "index.xhtml";
+    }
+    
+    public Part getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Part foto) {
+        this.foto = foto;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public void mostrarMsg(String titulo, String corpo){
+        FacesMessage mensagem = new FacesMessage(corpo);
+        mensagem.setSeverity(FacesMessage.SEVERITY_INFO);
+        FacesContext.getCurrentInstance().addMessage(titulo, mensagem);
+    }
+    
+}
---
+++ target/generated-sources/annotations/com/ifpb/seguranca/safechat/model/Usuario_.java
@@ -0,0 +1,17 @@
+package com.ifpb.seguranca.safechat.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-22T00:32:44")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> senha;
+    public static volatile SingularAttribute<Usuario, byte[]> foto;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> email;
+    public static volatile SingularAttribute<Usuario, String> username;
+
+}
